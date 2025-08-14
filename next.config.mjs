8c972104fea5fea4250eea13f1cb147f27c00794
Ankush Chhabra <ankushchhabra2002@gmail.com>
2025-08-14 12:33:56 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  experimental: {
+    optimizeCss: false, // for Turbopack, Tailwind works better with this off
+  },
+};
 export default nextConfig;
